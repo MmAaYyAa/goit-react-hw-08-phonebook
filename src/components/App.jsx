6f0d34edf8b8a52360172f3808ce2596266ddff3
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-12-24 18:41:41 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
 import React, {useEffect, lazy} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
-import { useDispatch, useSelector } from 'react-redux';
-import {getContacts} from '../redux/selectors';
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from '../redux/api';
-import ContactForm from './ContactForm/ContactForm';
-import ContactsList from './ContactsList/ContactsList';
-import Filter from './Filter/Filter';
+
 
 const HomePage = lazy(() => import('../pages/Home/Home'));
 const RegisterPage = lazy(() => import('../pages/Registrer/Register'));
@@ -14,7 +11,7 @@ const LoginPage = lazy(() => import('../pages/Login/Login'));
 
 export const App =  () => {
   const dispatch = useDispatch();
-  const { isLoading, error } = useSelector(getContacts);
+  //const { isLoading, error } = useSelector(getContacts);
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
@@ -35,8 +32,3 @@ export const App =  () => {
 }
 
 export default App;
-{/* <ContactForm/>
-<Filter />
-{isLoading && <b>Loading contacts...</b>}
-{error && <b>{error}</b>}
-<ContactsList/> */}
