3954678f01cb160dee7b67746496e79866b27623
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-12-25 00:17:13 +0100

    navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,51 @@
 import React, {useEffect, lazy} from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
 import { Layout } from './Layout';
 import { useDispatch } from 'react-redux';
-import { fetchContacts } from '../redux/api';
+import { refreshUser } from '../redux/auth/operations';
+import { useAuth } from '../hooks/useAuth';
 
 
 const HomePage = lazy(() => import('../pages/Home/Home'));
 const RegisterPage = lazy(() => import('../pages/Registrer/Register'));
 const LoginPage = lazy(() => import('../pages/Login/Login'));
+const ContactsPage = lazy(() => import('../pages/Contacts/Contacts'))
 
 export const App =  () => {
   const dispatch = useDispatch();
-  //const { isLoading, error } = useSelector(getContacts);
+  const { isRefreshing } = useAuth();
+
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
-    return (
-      <>
-        <div>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-         <Route index element={<HomePage/>}/>
-         <Route path="/register" element={<RegisterPage />}/>
-         <Route path="/login" element={<LoginPage />}/>
-        </Route>
-      </Routes>
-        </div>
-      </>
-    );
-}
-
-export default App;
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+      </Route>
+    </Routes>
+  );
+};
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import Header from './Header/Header';
+import {AppBar} from './AppBar/AppBar';
 
 export const Layout = () => {
     return (
-        <div>
-         <Header/>
-            <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
-        </div>
-    )
-}
+      <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+        <AppBar />
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </div>
+    );
+  };
