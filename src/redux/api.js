d6132c99e41ee1de9deec190226cbe773775e565
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2024-09-17 10:32:55 +0100

    change api

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,40 +1,43 @@
-import axios from "axios";
+import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.goit.global';
 
 export const fetchContacts = createAsyncThunk(
-    'contacts/fetchAll', 
-    async (_, thunkAPI) => {
-    try{
-        const response = await axios.get('/contacts');
-        return response.data;
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
     } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-      }
+      return thunkAPI.rejectWithValue(e.message);
     }
+  }
 );
 
 export const addContacts = createAsyncThunk(
-    'contacts/addContacts',
-    async ({ name, number }, thunkAPI) => {
-        try {
-            const response = await axios.post('/contacts', { name: name, number: number });
-            return response.data;
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
-        }
+  'contacts/addContacts',
+  async ({ name, number }, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', {
+        name: name,
+        number: number,
+      });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
+  }
 );
 
 export const deleteContacts = createAsyncThunk(
-    'contacts/deleteContacts',
-    async (contactsId, thunkAPI) => {
-        try {
-            const response = await axios.delete(`/contacts/${contactsId}`);
-            return response.data;
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
-        }
+  'contacts/deleteContacts',
+  async (contactsId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactsId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
+  }
 );
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,50 +1,49 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.goit.global';
 
 // Utility to add JWT
 const setAuthHeader = token => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  };
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
-  // Utility to remove JWT
+// Utility to remove JWT
 const clearAuthHeader = () => {
-    axios.defaults.headers.common.Authorization = '';
-  };
+  axios.defaults.headers.common.Authorization = '';
+};
 
-  
 //   POST /users/signup
 //  body: { name, email, password } - credentials
-  export const register = createAsyncThunk(
-    'auth/register',
-    async (credentials, thunkAPI) => {
-        try {
-            const res = await axios.post('/users/signup', credentials);
-            //if success - add token
-            setAuthHeader(res.data.token);
-            return res.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/signup', credentials);
+      //if success - add token
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
-  )
+  }
+);
 
 //   POST /users/login
 //  body: { email, password } - credentials
 export const logIn = createAsyncThunk(
-    'auth/login',
-    async (credentials, thunkAPI) => {
-        try {
-          const res = await axios.post('/users/login', credentials);
-          //if success - add token
-          setAuthHeader(res.data.token);
-          return res.data;
-        } catch (error) {
-          return thunkAPI.rejectWithValue(error.message);
-        }
-      }
-)
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/login', credentials);
+      //if success - add token
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 //POST /users/logout
 //headers: Authorization: Bearer token
